80a45555a452c6271c283c782fe39405628d60f0
40153557 <melliott21@qub.ac.uk>
2020-01-22 17:09:06 +0000

    dir bug fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-
-path = ".."
+path = "."
 
 # read in all filenames
 file_names = set()
@@ -8,13 +7,14 @@ for file in os.listdir(path):
     if file.endswith(".asc") | file.endswith(".prj"):
         file_names.add(file)
 
-
 # create needed dirs
 dirs = set()
 for file in file_names:
     dirs.add(file[:2])
 
-for this_dir in dirs:
+sorted_dirs = list(dirs)
+sorted(sorted_dirs)
+for this_dir in sorted_dirs:
     if not os.path.exists(path + "/" + this_dir):
         os.makedirs(path + "/" + this_dir)
 
